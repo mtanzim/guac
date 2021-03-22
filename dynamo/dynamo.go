6ecb44eec3385aa1a214d3d64497a958ebfa1c01
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-03-21 20:27:17 -0400

    parallelize

--- dynamo/dynamo.go
+++ dynamo/dynamo.go
@@ -3,6 +3,7 @@ package dynamo
 import (
 	"log"
 	"os"
+	"sync"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -15,6 +16,25 @@ type Item struct {
 	Data interface{}
 }
 
+func putData(item *Item, svc *dynamodb.DynamoDB, tableName string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	av, err := dynamodbattribute.MarshalMap(item)
+	if err != nil {
+		log.Println("Got error marshalling new waka item:")
+		log.Panicln(err.Error())
+	}
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String(tableName),
+	}
+	_, err = svc.PutItem(input)
+	if err != nil {
+		log.Println("Got error calling PutItem:")
+		log.Panicln(err.Error())
+	}
+	log.Println("Successfully added/updated date " + item.Date + " to table " + tableName)
+}
+
 func PutData(data map[string]interface{}) {
 	// log.Println(data)
 	sess, err := session.NewSession(&aws.Config{
@@ -25,24 +45,13 @@ func PutData(data map[string]interface{}) {
 	}
 	svc := dynamodb.New(sess)
 	tableName := os.Getenv("DYNAMO_TABLE")
+	var wg sync.WaitGroup
 	// create the input configuration instance
 	for k, v := range data {
 		item := Item{k, v}
-		av, err := dynamodbattribute.MarshalMap(item)
-		if err != nil {
-			log.Println("Got error marshalling new waka item:")
-			log.Panicln(err.Error())
-		}
-		input := &dynamodb.PutItemInput{
-			Item:      av,
-			TableName: aws.String(tableName),
-		}
-		_, err = svc.PutItem(input)
-		if err != nil {
-			log.Println("Got error calling PutItem:")
-			log.Panicln(err.Error())
-		}
-		log.Println("Successfully added/updated date " + item.Date + " to table " + tableName)
+		wg.Add(1)
+		go putData(&item, svc, tableName, &wg)
 	}
+	wg.Wait()
 
 }

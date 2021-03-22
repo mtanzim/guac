8cd77b705dfb9f31cce5a103953afa8846074185
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-03-21 20:14:20 -0400

    putItem to dynamoDB

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/mtanzim/guac
 
 go 1.16
 
-require github.com/joho/godotenv v1.3.0 // indirect
+require (
+	github.com/aws/aws-sdk-go v1.38.1 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+	golang.org/x/net v0.0.0-20210316092652-d523dce5a7f4 // indirect
+	golang.org/x/text v0.3.5 // indirect
+)
--- main.go
+++ main.go
@@ -2,13 +2,51 @@ package main
 
 import (
 	"log"
+	"os"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/mtanzim/guac/wakaApi"
 )
 
+type Item struct {
+	Date string
+	Data interface{}
+}
+
 func main() {
 	data := wakaApi.TransformData()
-	log.Println(data)
+	// log.Println(data)
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1"),
+	})
+	if err != nil {
+		log.Panicln(err)
+	}
+	svc := dynamodb.New(sess)
+	tableName := os.Getenv("DYNAMO_TABLE")
+	// create the input configuration instance
+	for k, v := range data {
+		item := Item{k, v}
+		av, err := dynamodbattribute.MarshalMap(item)
+		if err != nil {
+			log.Println("Got error marshalling new waka item:")
+			log.Panicln(err.Error())
+		}
+		// log.Println(av)
+		input := &dynamodb.PutItemInput{
+			Item:      av,
+			TableName: aws.String(tableName),
+		}
+		_, err = svc.PutItem(input)
+		if err != nil {
+			log.Println("Got error calling PutItem:")
+			log.Panicln(err.Error())
+		}
+		log.Println("Successfully added date " + item.Date + " to table " + tableName)
+	}
 
 }

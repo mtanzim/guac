d084b66fd8f08c14d3be8cc0bc259d76265260b5
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-04-05 14:41:57 -0400

    allow empty query for data endpoint

--- server/controllers/data.go
+++ server/controllers/data.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/mtanzim/guac/server/services"
 	"github.com/mtanzim/guac/server/utils"
@@ -15,10 +16,17 @@ func DataController(w http.ResponseWriter, req *http.Request) {
 
 	reqStart := req.URL.Query().Get("start")
 	reqEnd := req.URL.Query().Get("end")
-	if err := utils.ValidateQueryDate(reqStart, reqEnd); err != nil {
-		utils.HandlerError(w, err)
-		return
+
+	if reqStart == "" && reqEnd == "" {
+		reqStart, reqEnd = os.Getenv("START"), os.Getenv("END")
+
+	} else {
+		if err := utils.ValidateQueryDate(reqStart, reqEnd); err != nil {
+			utils.HandlerError(w, err)
+			return
+		}
 	}
+
 	rv := services.DataService(reqStart, reqEnd)
 	if err := json.NewEncoder(w).Encode(rv); err != nil {
 		log.Println(err)

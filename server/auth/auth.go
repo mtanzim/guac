02e57887a9606850ffcb31ba63066fffc07a966d
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-04-29 22:39:30 -0400

    jwt token auth

--- server/auth/auth.go
+++ server/auth/auth.go
@@ -2,30 +2,51 @@ package auth
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 	"os"
+	"strings"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
 )
+
+type MyCustomClaims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
 
 // TODO: verify auth, and return a token
 func AuthVerify(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 
-		expectedUser, expectedPass := os.Getenv("USERNAME"), os.Getenv("USERPASS")
-		if expectedUser == "" || expectedPass == "" {
+		expectedUser, secret := os.Getenv("USERNAME"), os.Getenv("SECRET")
+		if expectedUser == "" {
 			w.WriteHeader(http.StatusInternalServerError)
 			json.NewEncoder(w).Encode("User/password env not setup")
 			return
 		}
 
-		// TODO: do not pass passwords in the query params!
-		user := req.URL.Query().Get("user")
-		pass := req.URL.Query().Get("pass")
-
-		if user == expectedUser && pass == expectedPass {
-			next.ServeHTTP(w, req)
+		reqToken := req.Header.Get("Authorization")
+		splitToken := strings.Split(reqToken, "Bearer")
+		if len(splitToken) != 2 {
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode("Malformed token")
 			return
 		}
+		parsedToken := strings.TrimSpace(splitToken[1])
+		token, err := jwt.ParseWithClaims(parsedToken, &MyCustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+			return []byte(secret), nil
+		})
+		if claims, ok := token.Claims.(*MyCustomClaims); ok && token.Valid {
+			log.Printf("%v %v", claims.Username, claims.StandardClaims.ExpiresAt)
+			if claims.Username == expectedUser && claims.ExpiresAt > (time.Now().Unix()*1000) {
+				next.ServeHTTP(w, req)
+				return
+			}
+		}
+		log.Println(err)
 		w.WriteHeader(http.StatusForbidden)
 		json.NewEncoder(w).Encode("Unauthorized")
 
--- server/controllers/login.go
+++ server/controllers/login.go
@@ -5,6 +5,11 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
+
+	"github.com/mtanzim/guac/server/auth"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
 type LoginArg struct {
@@ -19,10 +24,10 @@ func LoginController(w http.ResponseWriter, req *http.Request) {
 	}
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 
-	expectedUser, expectedPass := os.Getenv("USERNAME"), os.Getenv("USERPASS")
-	if expectedUser == "" || expectedPass == "" {
+	expectedUser, expectedPass, secret := os.Getenv("USERNAME"), os.Getenv("USERPASS"), os.Getenv("SECRET")
+	if expectedUser == "" || expectedPass == "" || secret == "" {
 		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode("User/password env not setup")
+		json.NewEncoder(w).Encode("user/password/secret env not setup")
 		return
 	}
 
@@ -33,11 +38,26 @@ func LoginController(w http.ResponseWriter, req *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	log.Println(expectedUser, expectedPass)
-	log.Println(t)
 	if t.Username == expectedUser && t.Password == expectedPass {
+		claims := auth.MyCustomClaims{
+			t.Username,
+			jwt.StandardClaims{
+				// in ms
+				ExpiresAt: (time.Now().Unix() * 1000) + time.Hour.Milliseconds(),
+				Issuer:    "guac",
+			},
+		}
+
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+		tokenString, err := token.SignedString([]byte(secret))
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode("Failed to sign token")
+			return
+		}
 		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode("Token")
+		json.NewEncoder(w).Encode(tokenString)
 		return
 	}
 	w.WriteHeader(http.StatusForbidden)

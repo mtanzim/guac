fbd63acc741e194312808a99fae10c99b758a0ca
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-04-29 22:49:34 -0400

    change base url

--- server/app.go
+++ server/app.go
@@ -9,12 +9,17 @@ import (
 	"github.com/mtanzim/guac/server/controllers"
 )
 
+var (
+	BaseURL = "/api/v1"
+)
+
 func Start() {
-	http.HandleFunc("/health", controllers.HealthController)
-	http.HandleFunc("/login", controllers.LoginController)
-	http.Handle("/", auth.AuthVerify(http.HandlerFunc(controllers.RootController)))
-	http.Handle("/data", auth.AuthVerify(http.HandlerFunc(controllers.DataController)))
-	http.Handle("/plot", auth.AuthVerify(http.HandlerFunc(controllers.PlotController)))
+
+	http.HandleFunc(BaseURL+"/health", controllers.HealthController)
+	http.HandleFunc(BaseURL+"/login", controllers.LoginController)
+	http.Handle(BaseURL+"/", auth.AuthVerify(http.HandlerFunc(controllers.RootController)))
+	http.Handle(BaseURL+"/data", auth.AuthVerify(http.HandlerFunc(controllers.DataController)))
+	http.Handle(BaseURL+"/plot", auth.AuthVerify(http.HandlerFunc(controllers.PlotController)))
 
 	port := os.Getenv("REST_PORT")
 	if port == "" {

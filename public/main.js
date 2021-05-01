ccdc53d22863a46313e0093dddf16a04f602cb86
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-05-01 14:48:55 -0400

    token storage in localstorage

--- public/main.js
+++ public/main.js
@@ -1,5 +1,7 @@
 import { plotLangPie } from "./modules/plot.js";
 
+const TOKEN_KEY = "WakaToken";
+
 function login() {
   const username = document.getElementById("username").value;
   const password = document.getElementById("pass").value;
@@ -16,6 +18,7 @@ function login() {
       if (!token) {
         throw new Error("Unable to login");
       }
+      window.localStorage.setItem(TOKEN_KEY, token);
       return token;
     });
 }
@@ -33,19 +36,30 @@ function plotData(data) {
   plotLangPie(percentages, "lang-pie");
 }
 
-window.initWaka = () =>
-  login()
-    .then((token) => {
-      const loginForm = document.getElementById("login-form");
-      loginForm.remove();
-      return fetch("/api/v1/data", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
+function fetchData(token) {
+  const loginForm = document.getElementById("login-form");
+  loginForm.remove();
+  return fetch("/api/v1/data", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }).then((res) => res.json());
+}
+
+function initWaka() {
+  const curToken = window.localStorage.getItem(TOKEN_KEY);
+  if (curToken) {
+    return fetchData(curToken).then(plotData);
+  }
+
+  const loginBtn = document.getElementById("login-btn");
+  loginBtn.onclick = () =>
+    login()
+      .then((token) => fetchData(token))
+      .then(plotData)
+      .catch((err) => {
+        document.getElementById("error").innerText = err.message;
       });
-    })
-    .then((res) => res.json())
-    .then(plotData)
-    .catch((err) => {
-      document.getElementById("error").innerText = err.message;
-    });
+}
+
+window.initWaka = initWaka;

99d0c93081a6e4ef57d371b456b0d962a296872f
Tanzim Mokammel <mtanzim@gmail.com>
2024-07-27 13:49:37 -0400

    delete commented code

--- client-v2/src/utils.ts
+++ client-v2/src/utils.ts
@@ -11,18 +11,6 @@ export const formatDateForReq = (date: Date) => {
   return date.toISOString().split("T")[0];
 };
 
-// export function getDateRange(days: number) {
-
-
-//   const endDate = new Date();
-//   endDate.setDate(endDate.getDate() - 1);
-//   const ending = formatDateForReq(endDate);
-//   const startDate = new Date();
-//   startDate.setDate(startDate.getDate() - days);
-//   const starting = formatDateForReq(startDate);
-//   return { starting, ending };
-// }
-
 export function getColor(lang: string): string {
   return (
     (colors as Record<string, { color: string | null }>)?.[lang]?.color ||

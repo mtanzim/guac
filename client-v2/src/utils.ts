0d2f19f3ae73b5eea85f8bdb57e3cce86920ee0d
Tanzim Mokammel <mtanzim@gmail.com>
2024-07-29 20:54:55 -0400

    fix build

--- client-v2/src/utils.ts
+++ client-v2/src/utils.ts
@@ -20,6 +20,6 @@ export function getColor(lang: string): string {
 
 export const DEFAULT_DAY_RANGE = 7;
 export const TOKEN_KEY = "WakaToken";
-export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = import.meta.env.PROD ? "" : "http://localhost:8080";
 export const TOP_N_LANGUAGES = 10;
 export const TOP_N_PROJECTS = 7;

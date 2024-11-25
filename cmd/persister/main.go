f9457e582a21b47e56d196063299190ee612bcf3
Tanzim Mokammel <20617008+mtanzim@users.noreply.github.com>
2024-11-24 19:46:06 -0500

    fix; cron action v1 (#34)

--- cmd/persister/main.go
+++ cmd/persister/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/mtanzim/guac/firestoreClient"
 	"github.com/mtanzim/guac/wakaApi"
@@ -8,5 +10,7 @@ import (
 
 func main() {
 	data := wakaApi.TransformData()
+	log.Println("successfully got api data")
 	firestoreClient.PutData(data)
+	log.Println("successfully placed data on firestore")
 }
